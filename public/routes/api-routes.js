1c8e2314f28f5437d8e57df3c70fd646b00064bd
Ana Luna <luna.ana92@gmail.com>
2020-12-14 23:54:04 -0800

    deploying

--- public/routes/api-routes.js
+++ public/routes/api-routes.js
@@ -32,40 +32,17 @@ router.get("/books", (req, res) => {
         })
 });
 
-router.delete('/bookid', (req, res) => {
-    BookModel.remove(
-          {
-                id: req.query.id
-          },
-          function(err) {
-                if (err) res.send(err);
-                else res.send('Successfully! Book has been Deleted.');
-          }
-    );
-});
+// router.delete('/bookid', (req, res) => {
+//     BookModel.remove(
+//           {
+//                 id: req.query.id
+//           },
+//           function(err) {
+//                 if (err) res.send(err);
+//                 else res.send('Successfully! Book has been Deleted.');
+//           }
+//     );
+// });
 
 module.exports = router;
 
-
-
-// module.exports = function (app) {
-
-//     app.post("/book", async (req, res) => {
-//         const savedBook = new Books({
-//             title: req.body.title,
-//             authors: req.body.authors,
-//             description: req.body.description,
-//             image: req.body.image,
-//             link: req.body.link,
-//         });
-//         await savedBook.save();
-//         res.send("Book Saved!");
-//     });
-
-//     app.get("/books", (req, res) => {
-//         Books.find({})
-//             .then((books) => {
-//                 res.json(books);
-//             }) 
-//     });
-// }
